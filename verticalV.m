ece114012f4bc9a501a8584da945fd0461d0d1f4
atlantis73 <atlantis.sts.us.73@gmail.com>
2015-02-03 10:28:52 -0500

    Changed code

--- verticalV.m
+++ verticalV.m
@@ -1,14 +1,13 @@
-function verticalV(dataFname)  
 
+dataFname = 'Altimeter 1 Data - Subscale - Dec 13';
 datalist = xlsread(dataFname); % gets data from the excel file of the alt data  
 
-time = datalist(5:end,1);
-altitude = datalist(5:end,2);
+time = datalist(5:end,1)';
+altitude = datalist(5:end,2)';
 altitude = altitude * 0.3048;
 
-v = (altitude(2:end) - altitude(1:end-1))./(time(2:end) - time(1:end-1));
-v = [0;v];
-data = [time v];
+v = diff(altitude)./diff(time);
+v = [0 v];
+data = [time;v];
 
-plot(data(:,1),data(:,2))
-end
+plot(data(1,:),data(2,:));
